7f2186956bca74d65832a8d3eb14e11d90ce1745
oroni <brianoroni6@gmail.com>
2017-02-09 10:17:14 +0300

    Initial commit

---
+++ posts/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from django.contrib import admin
+
+from posts import views
+
+
+urlpatterns = (
+    url(r'^$', views.post_list),
+    url(r'^create/$', views.post_create),
+    url(r'^(?P<id>\d+)/$', views.post_detail, name='detail'),
+    url(r'^(?P<id>\d+)/edit/$', views.post_update, name='update'),
+    url(r'^delete/$', views.post_delete),
+	# url(r'^posts/$', "<appname>.views.<function_name>"),
+)
---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pid=$(ps aux | grep "./manage.py runserver" | grep -v grep | head -1 | xargs | cut -f2 -d" ")
+
+if [[ -n "$pid" ]]; then
+    kill $pid
+fi
+
+fuser -k 8000/tcp
+./manage.py runserver
